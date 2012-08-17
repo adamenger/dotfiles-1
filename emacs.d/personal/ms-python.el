8f6a96c994343b4d98af588e1735855409b393e1
Mike Steder <steder@gmail.com>
2012-08-16 21:47:13 -0500

    Getting rope and pymacs working

--- emacs.d/personal/ms-python.el
+++ emacs.d/personal/ms-python.el
@@ -25,19 +25,7 @@
 ;; Pymacs
 ;; to install cd to ~/etc/emacs.d/thirdparty/Pymacs and run
 ;; $ make && python setup.py install
-;; (add-to-list 'load-path "~/etc/emacs.d/thirdparty/Pymacs")
-;; (require 'pymacs)
-;; ;; TODO: do this setup only if python mode is activated
-;; (if (and (getenv "PYTHONPATH") (not (string= (getenv "PYTHONPATH") "")))
-;;     ;; append at the end with separator
-;;     (setenv "PYTHONPATH"
-;;             (concat
-;;              (getenv "PYTHONPATH") path-separator
-;;              (expand-file-name "~/etc/emacs.d/thirdparty/python/")))
-;;   ;; else set it without separator
-;;   (setenv "PYTHONPATH"
-;;           (expand-file-name "~/etc/emacs.d/thirdparty/python/"))
-;;   )
-;; (message "PYTHONPATH: %s" (getenv "PYTHONPATH"))
-;; (pymacs-load "ropemacs" "rope-")
-;; (setq ropemacs-enable-autoimport t)
+(add-to-list 'load-path "~/etc/emacs.d/thirdparty/Pymacs")
+(require 'pymacs)
+(pymacs-load "ropemacs" "rope-")
+(setq ropemacs-enable-autoimport t)
