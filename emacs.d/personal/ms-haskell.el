e3274722fc9ab0c2268f434b5f025e7d11e84bea
Mike Steder <steder@gmail.com>
2012-11-07 23:16:48 -0600

    adding haskell config

---
+++ emacs.d/personal/ms-haskell.el
@@ -0,0 +1,17 @@
+;;
+;; haskell mode stuff
+;;
+
+;; there are three indentation modes:
+;; haskell-indentation
+;; haskell-indent
+;; haskell-simple-indent
+
+(add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
+
+;; ghc mode installs a bunch of useful stuff for haskell
+;; to support syntax highlighting and flymake
+;; you may need to install 'ghc-mod' i.e.:
+;; cabal install ghc-mod
+(autoload 'ghc-init "ghc" nil t)
+(add-hook 'haskell-mode-hook (lambda () (ghc-init)))
