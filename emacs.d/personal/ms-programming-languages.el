797f71276185e4209a40c54007080f2f7cde5620
Mike Steder <steder@gmail.com>
2012-12-19 11:49:28 -0600

    adding gently improved ruby config

--- emacs.d/personal/ms-programming-languages.el
+++ emacs.d/personal/ms-programming-languages.el
@@ -8,15 +8,7 @@
 (load "ms-test")
 (load "ms-javascript")
 (load "ms-snippets")
-
-;; Ruby crap:
-(autoload 'ruby-mode "ruby-mode" "Major mode for ruby files" t)
-(add-to-list 'auto-mode-alist '("\\.rb$" . ruby-mode))
-(add-to-list 'auto-mode-alist '("Gemfile" . ruby-mode))
-(add-to-list 'auto-mode-alist '("Rakefile" . ruby-mode))
-(add-to-list 'auto-mode-alist '("\\.rake\\'" . ruby-mode))
-(add-to-list 'auto-mode-alist '("\\.rb\\'" . ruby-mode))
-(add-to-list 'interpreter-mode-alist '("ruby" . ruby-mode))
+(load "ms-ruby")
 
 ;; PYFLAKES
 ;; configure hooks to run pyflakes automatically with flymake mode:
